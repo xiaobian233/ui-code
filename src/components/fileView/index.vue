c91ce140bba4ea32c104beb24ba43dbe0c767eec
xiaobian233 <1282845084@qq.com>
2022-06-30 11:45:06 +0800

    feat: useModal调用bug修复

--- src/components/fileView/index.vue
+++ src/components/fileView/index.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import { reactive } from 'vue'
+import { reactive, watch } from 'vue'
 export default {
   name: 'w-fileView',
   props: ['data'],
@@ -14,7 +14,11 @@ export default {
     const isAudio = (path) => /\.(opus|flac|webm|weba|wav|ogg|m4a|mp3|oga|mid|amr|aiff|wma|au|aac)/.test(path);
     const isVideo = (path) => /\.(mp4|avi|wmv|mpg|mpeg|mov|rm|ram|swf|flv)/.test(path);
     const isImage = (path) => /\.(xbm|tif|pjp|svgz|jpg|jpeg|ico|tiff|gif|svg|jfif|webp|png|bmp|pjpeg|avif)/.test(path);
-
+    watch(props.data, () => {
+      data.map(x => {
+        console.error(x)
+      })
+    }, { immediate: true, deep: true })
 
   }
 }
