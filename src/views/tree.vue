57fc0d895c60a08fd1a91e996efd8df0fae827ba
xiaobian233 <1282845084@qq.com>
2022-08-31 13:28:10 +0800

    fix: tree代码优化及复选框部分样式开发

--- src/views/tree.vue
+++ src/views/tree.vue
@@ -3,7 +3,8 @@ import { computed, defineComponent, ref, onMounted, reactive, watchEffect } from
 export default defineComponent({
     name: 'w-tree',
     props: {
-        data: { type: Array, default: [] }
+        data: { type: Array, default: [] },
+        isOpen: { type: Boolean, default: false }
     },
     setup(props, { emit }) {
         const datas = reactive({
@@ -31,61 +32,60 @@ export default defineComponent({
             ],
             treeData: [],
             childData: [],
-            keyBol: true
+            keyBol: props.isOpen,
+            floatArr: []
         })
-        const opt = {
-            child: false,
-            tier: 2,
-        }
-
         const clickTreeItem = (index, item) => {
-            let b = false;
             if (index == 1 && item.children && item.children.length > 0) {
-                b = !item.childShow
-                item.childShow = b
+                const isK = k => k == 'K'
+                const areE = _ => 'E'
+                isK(item.open) ? (item.open = 'E') : (item.open = 'K')
+                let b = isK(item.open)
                 const f = (arr, b) => {
                     arr.forEach(x => {
-                        x.isShow = b
-                        if (!item.childShow)
-                            if (x.children && x.children.length > 0) {
-                                f(x.children, b)
-                            }
+                        if (x.open && isK(x.open)) {
+                            x.isShow = b
+                            x.open = areE()
+                        }
+                        else x.isShow = b
+                        if (x.children && x.children.length > 0) {
+                            f(x.children, b ? !x.isShow : b)
+                        }
                     })
                 }
                 f(item.children, b)
             }
+            if (index == 2) {
+                item.check = !item.check
+            }
         }
 
-        const getters = (index, item) => {
-            if (index == 1 && item.children && item.children.length > 0) return 'K'
-            else return ''
-        }
-        const getDatas = computed(() => {
-            let floatArr = []
+        const init = () => {
             let arrFn = (arr, index, parent = null) => (
                 arr.forEach(x => {
                     x.isShow = datas.keyBol
-                    x.childShow = datas.keyBol
-                    if (index == 0) x.index = index
-                    if (parent) x.parent = parent
+                    x.index = index
+                    parent && (x.parent = parent)
                     if (parent && parent.tier) x.tier = parent.tier + 1
                     else x.tier = 2
                     if (x.children && x.children.length > 0) {
-                        arrFn(x.children, index++, x)
+                        // 统一走状态为 开启
+                        x.isShow ? x.open = 'E' : x.open = 'K'
+                        arrFn(x.children, ++index, x)
                     }
-                    floatArr.push(x)
+                    // 判断第一条数据
+                    if (x.index == 0 && !x.isShow && !datas.keyBol) x.isShow = true, x.open = 'E'
+                    datas.floatArr.push(x)
                 })
             )
             arrFn(datas.data, 0)
-            floatArr = floatArr.reverse()
-            console.error(floatArr, 'floatArr floatArr floatArr');
-            return floatArr
-        })
+            datas.floatArr = datas.floatArr.reverse()
+            // console.error(datas.floatArr, 'datas.floatArr');
+        }
+        init()
         return {
             datas,
             clickTreeItem,
-            getters,
-            getDatas
         }
     }
 })
@@ -93,20 +93,15 @@ export default defineComponent({
 
 <template>
     <div class="w-tree">
-        <template v-for="item, index in getDatas" :key="index">
+        <template v-for="item, index in datas.floatArr" :key="index">
             <div class="w-tree-item" v-if="item.isShow">
-                <span :class="{ 'w-tree-item-open': i == 1 && item.children && item.children.length > 0 }"
-                    v-for="i in item.tier" :key="i" @click="() => clickTreeItem(i, item)" class="w-tree-tier">{{  i == 1
-                    ? getters(i, item) : ''
-
-
-
-
-
-
-
-
-                    }}</span>
+                <span class="w-tree-tier" :class="{
+                    'w-tree-item-open': i == 1 && item.children && item.children.length > 0,
+                    'w-tree-isCheck': i == 2,
+                    'w-tree-selected': item.check
+                }" v-for="i in item.tier" :key="i" @click="() => clickTreeItem(i, item)">
+                    <span v-if="i == 1">{{  item.open  }}</span>
+                </span>
                 <span>{{  item.title  }}</span>
             </div>
         </template>
@@ -115,6 +110,8 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .w-tree {
+    margin: 36px;
+
     .w-tree-item {
         display: flex;
         align-items: flex-start;
@@ -131,9 +128,25 @@ export default defineComponent({
             text-align: center;
         }
 
+        .w-tree-isCheck {
+            background-color: #eaf362;
+        }
+
+        .w-tree-isCheck.w-tree-selected {
+            &::before {
+                content: '';
+                width: 8px;
+                height: 8px;
+                display: inline-block;
+                border-radius: 50%;
+                background-color: red;
+            }
+        }
+
         .w-tree-item-open {
             background-color: pink;
         }
+
     }
 
 }
