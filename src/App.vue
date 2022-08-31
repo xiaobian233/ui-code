f80f69ee9d2387f5c940aa1c0c65cc9f3cbefeb7
xiaobian233 <1282845084@qq.com>
2022-08-31 11:43:22 +0800

    fix: 虚拟滚动优化

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
-  html,body,#app{
-    height: 100vh;
-  }
+html,
+body,
+#app {
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
 </style>
